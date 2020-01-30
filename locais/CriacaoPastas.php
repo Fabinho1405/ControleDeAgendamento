a2ccd2d4e6a31b46ff9ce9706dd2d049a355bed4
Fabio <fabinho1405@gmail.com>
2020-01-30 16:26:50 -0300

    Modificacao de Pastas

--- locais/CriacaoPastas.php
+++ locais/CriacaoPastas.php
@@ -17,7 +17,7 @@
 		$contrato=$rowMateriais->contrato_cc;
 		$material=$rowMateriais->material_cc;
 		//echo $rowMateriais->contrato_cc."-".$rowMateriais->material_cc."<br>";
-		if(mkdir('X:\EDIÇÃO\SEPARADAS\++ ENVIOS SISTEMA ++\2020\16.01 - SISTEMA\ '.$contrato.'-'.$material.'')){
+		if(mkdir('X:\EDIÇÃO\SEPARADAS\++ ENVIOS SISTEMA ++\2020\29.01 - SISTEMA\ '.$contrato.'-'.$material.'')){
 			echo "criada com sucesso - ".$contrato."<br />";
 		}else{
 			echo "já existe<br>";  
